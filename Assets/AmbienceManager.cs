4b786c06be879a913adc7fcb31e092320342b1dc
cdom <dominguerilla@gmail.com>
2018-04-18 20:57:17 -0400

    added ambient music

--- Assets/AmbienceManager.cs
+++ Assets/AmbienceManager.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AmbienceManager : MonoBehaviour {
     
     public Material Skybox1, Skybox2;
     public ParticleSystem pSystem1, pSystem2;
-    
+    public AudioClip music; // the ambient music to loop
+
     public bool isSkybox1 = true;
 
+    private AudioSource aSource;
+
     public void Start() {
+        aSource = GetComponent<AudioSource>();
+        aSource.clip = music;
+        aSource.loop = true;
+        aSource.Play();
+
         if(!Skybox1 || !Skybox2 || !pSystem1 || !pSystem2) {
             Debug.LogError("Skyboxes and particle systems not set!");
             Destroy(this);
@@ -29,12 +38,23 @@ public class AmbienceManager : MonoBehaviour {
             RenderSettings.skybox = Skybox2;
             pSystem1.Stop();
             pSystem2.Play();
+            StopMusic();
             isSkybox1 = false;
         }else {
             RenderSettings.skybox = Skybox1;
             pSystem2.Stop();
             pSystem1.Play();
+            StartMusic();
             isSkybox1 = true;
         }
     }    
+
+    public void StartMusic() {
+        if(aSource.clip)
+            aSource.Play();
+    }
+
+    public void StopMusic() {
+        aSource.Stop();
+    }
 }
