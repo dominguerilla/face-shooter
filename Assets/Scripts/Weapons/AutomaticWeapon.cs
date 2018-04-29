5fb6a53f061739b8ddaff0645bde59ddef6b47d6
cdom <dominguerilla@gmail.com>
2018-04-29 16:16:14 -0400

    renamed uzi.cs

--- Assets/Scripts/Weapons/AutomaticWeapon.cs
+++ Assets/Scripts/Weapons/AutomaticWeapon.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 
-public class Uzi : Gun {
+public class AutomaticWeapon : Gun {
 
     public override string attachmentPoint
     {
